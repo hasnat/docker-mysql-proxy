23c7747b1f10b4c700b189c579d776f86666f6d2
Hasnat <hasnat@mac.com>
2024-09-01 19:55:58 +0100

    Update log.lua
    include client src address in log

--- log.lua
+++ log.lua
@@ -34,8 +34,9 @@ local fh = io.open(log_file, "a+")
 function read_query( packet )
     if string.byte(packet) == proxy.COM_QUERY then
         local query = string.sub(packet, 2)
-        fh:write( string.format("%s %6d -- %s \n", 
+        fh:write( string.format("%s %s T%6d -- %s \n", 
             os.date('%Y-%m-%d %H:%M:%S'), 
+            proxy.connection.client.src.address,
             proxy.connection.server["thread_id"], 
             query)) 
         fh:flush()
